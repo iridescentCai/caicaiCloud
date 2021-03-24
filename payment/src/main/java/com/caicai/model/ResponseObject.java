4c1478ff03b301a1970222c91789b82f2c6e04d4
iridescentCai <872462554@qq.com>
2021-03-25 01:24:56 +0800

    ResponseObject

--- payment/src/main/java/com/caicai/model/ResponseObject.java
+++ payment/src/main/java/com/caicai/model/ResponseObject.java
@@ -1,4 +1,26 @@
 package com.caicai.model;
 
-public class ResponseObject {
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseObject<T> {
+    private final Integer successCode = 1;
+    private final Integer failCode = -1;
+
+    private Integer stateCode;
+
+    private Object data;
+
+    public ResponseObject getSuccessResponse(Object data) {
+        return new ResponseObject(successCode, data);
+    }
+
+    public ResponseObject getFailResponse() {
+        return new ResponseObject(failCode, null);
+    }
 }
