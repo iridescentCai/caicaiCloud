ed168cb96b4df333f3cb8e8097e1836beae548c1
iridescentCai <872462554@qq.com>
2021-03-26 22:57:16 +0800

    eureka cluster coding

---
+++ eureka8762/src/main/java/com/caicai/EurekaApplication.java
@@ -0,0 +1,22 @@
+package com.caicai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author caicai
+ * @Date 2021/3/26 上午12:37
+ */
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args){
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
+
+    
+
---
+++ eureka8763/src/main/java/com/caicai/eurekaApplication.java
@@ -0,0 +1,12 @@
+package com.caicai;
+
+/**
+ * @author caicai
+ * @Date 2021/3/26 下午10:42
+ */
+
+public class eurekaApplication {
+}
+
+    
+
