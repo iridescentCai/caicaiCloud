1d639e90e5f8b15a59cbba258460aad65202eecb
iridescentCai <872462554@qq.com>
2021-04-08 07:54:37 +0800

    nacos test

---
+++ payment9001/src/main/java/com/caicai/paymentApplication.java
@@ -0,0 +1,12 @@
+package com.caicai;
+
+/**
+ * @author caicai
+ * @Date 2021/4/7 下午9:26
+ */
+
+public class paymentApplication {
+}
+
+    
+
---
+++ payment9002/src/main/java/com/caicai/paymentApplication.java
@@ -0,0 +1,12 @@
+package com.caicai;
+
+/**
+ * @author caicai
+ * @Date 2021/4/7 下午9:38
+ */
+
+public class paymentApplication {
+}
+
+    
+
